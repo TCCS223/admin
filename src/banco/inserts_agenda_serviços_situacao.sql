d68b6ef006a285d23400a8baa4a3543b11020f82
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-08-01 22:49:28 -0300

    commit

--- src/banco/inserts_agenda_serviços_situacao.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO agenda_servicos_situacao (agend_serv_situ_nome) VALUES ('Pendente');
-INSERT INTO agenda_servicos_situacao (agend_serv_situ_nome) VALUES ('Em Andamento');
-INSERT INTO agenda_servicos_situacao (agend_serv_situ_nome) VALUES ('Concluído');
-INSERT INTO agenda_servicos_situacao (agend_serv_situ_nome) VALUES ('Cancelado');
--- src/banco/inserts_categorias.sql
+++
@@ -1,5 +0,0 @@
-insert into categorias (cat_id, cat_nome) VALUES
-                        (1, 'Caminhao'),
-                        (2, 'Carro'),
-                        (3, 'Moto');
-                       
