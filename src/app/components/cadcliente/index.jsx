99d11a8f5fbdfa41c0ac8424c4d5bccb7695e388
NeiJunio <neijunio50@gmail.com>
2024-08-03 19:06:47 -0300

    commit

--- src/app/components/cadcliente/index.jsx
+++ src/app/components/cadcliente/index.jsx
@@ -13,8 +13,8 @@ export default function CadCliente() {
 
     const handleselectSexo = () => {
         const sexo = selectSexo.current.value;
-
         console.log(sexo)
+
     }
 
     const Pesquisar = () => {
@@ -63,7 +63,7 @@ export default function CadCliente() {
         <div id="clientes" className={`${styles.content_section}`}>
             <h2 className={styles.title_page}>Gerenciamento de Clientes</h2>
             <div className={styles.button_group}>
-            <button id="novoCliente" onClick={Novo}>Novo</button>
+                <button id="novoCliente" onClick={Novo}>Novo</button>
                 <button id="alterarCliente">Alterar</button>
                 <button id="excluirCliente">Excluir</button>
                 <button id="localizarCliente" onClick={Pesquisar}>Localizar</button>
@@ -80,23 +80,23 @@ export default function CadCliente() {
                     </div>
 
                     <div className={`${styles.grid_item} ${styles.grid_nome}`}>
-                        <label for="nome_cliente" className={styles.label_cliente} >Nome:</label>
+                        <label for="nome_cliente" className={styles.label_cliente} >Nome</label>
                         <input type="text" id="nome_cliente" name="nome_cliente" required className={styles.input_cliente} placeholder="Nome Completo" />
                     </div>
 
                     <div className={`${styles.grid_item} ${styles.grid_cpf}`}>
-                        <label for="cpf_cliente" className={styles.label_cliente}>CPF:</label>
+                        <label for="cpf_cliente" className={styles.label_cliente}>CPF</label>
                         <input type="text" id="cpf_cliente" name="cpf_cliente" required className={styles.input_cliente} placeholder="xxx.xxx.xxx - xx" />
                     </div>
                 </div>
 
                 <div className={`${styles.grid_item} ${styles.grid_data}`}>
-                    <label for="data_nasc_cliente" className={styles.label_cliente}>Data de nascimento:</label>
+                    <label for="data_nasc_cliente" className={styles.label_cliente}>Data de nascimento</label>
                     <input type="date" id="data_nasc_cliente" name="data_nasc_cliente" required className={styles.input_cliente} />
                 </div>
 
                 <div className={`${styles.grid_item} ${styles.grid_sexo}`}>
-                    <label for="sexo_cliente" className={styles.label_cliente}>Sexo:</label>
+                    <label for="sexo_cliente" className={styles.label_cliente}>Sexo</label>
                     <select ref={selectSexo} onClick={handleselectSexo} id="sexo_cliente" name="sexo_cliente" required className={`${styles.select_cliente} ${styles.input_sexo}`}>
                         <option value="" disabled selected>Selecionar</option>
                         <option value="0">Masculino</option>
@@ -106,7 +106,7 @@ export default function CadCliente() {
                 </div>
 
                 <div className={`${styles.grid_item} ${styles.grid_acesso}`}>
-                    <label for="nivel_acesso" className={styles.label_cliente}>Nível de Acesso:</label>
+                    <label for="nivel_acesso" className={styles.label_cliente}>Nível de Acesso</label>
                     <select id="nivel_acesso" name="nivel_acesso"
                         className={`${styles.select_cliente} ${styles.input_acesso}`}>
                         <option value="0" className={styles.option}>Usuário</option>
@@ -114,20 +114,33 @@ export default function CadCliente() {
                     </select>
                 </div>
 
+                
+
 
                 <div className={`${styles.grid_item} ${styles.grid_telefone}`}>
-                    <label for="telefone_cliente" className={styles.label_cliente}>Telefone:</label>
+                    <label for="telefone_cliente" className={styles.label_cliente}>Telefone</label>
                     <input type="tel" id="telefone_cliente" name="telefone_cliente" required className={` ${styles.input_cliente}`} placeholder="(xx) xxxxx - xxxxx" />
                 </div>
 
                 <div className={`${styles.grid_item} ${styles.grid_email}`}>
-                    <label for="email_cliente" className={styles.label_cliente}>Email:</label>
+                    <label for="email_cliente" className={styles.label_cliente}>Email</label>
                     <input type="email_cliente" id="email_cliente" name="email_cliente" required className={styles.input_cliente} placeholder="exemplo@exemplo.com" />
                 </div>
                 <div className={`${styles.grid_item}  ${styles.grid_observacoes}`}>
                     <label htmlFor="observacoes_cliente" className={styles.label_cliente}>Observações</label>
                     <input type="text" id="observacoes_cliente" name="observacoes_cliente" required className={styles.input_cliente} />
                 </div>
+
+                <div className={`${styles.grid_item} ${styles.grid_situacao}`}>
+                    <label for="situacao_cliente" className={styles.label_cliente}>Situação</label>
+                    <select id="situacao_cliente" name="situacao_cliente"
+                        className={`${styles.select_cliente} ${styles.input_situacao}`}>
+                        <option value="ativo" className={styles.option} selected>Ativo</option>
+                        <option value="inativo" className={styles.option}>Inativo</option>
+                    </select>
+                </div>
+
+                
             </form>
 
             <div className={styles.footer_form}>
@@ -136,6 +149,8 @@ export default function CadCliente() {
             </div>
 
             {/* MODAL */}
+
+
         </div>
     );
 }
--- src/app/components/servicos/index.jsx
+++ src/app/components/servicos/index.jsx
@@ -3,7 +3,7 @@ import styles from './index.module.css';
 export default function Servicos() {
     return (
         <div id="servicos" className={`${styles.content_section}`}>
-            <h2 className={styles.h2}>Gerenciamento de Serviços</h2>
+            <h2 className={styles.title_page}>Gerenciamento de Serviços</h2>
             <div className={styles.button_group}>
                 <button id="novoServico">Novo</button>
                 <button id="alterarServico">Alterar</button>
@@ -15,39 +15,83 @@ export default function Servicos() {
                 <input type="hidden" id="servicoId" className={styles.input_servicos} />
 
                 <div className={styles.grid}>
-                    <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
+                    <div className={`${styles.grid_item} ${styles.grid_codigo}`}>
                         <label for="codigo_servico" className={styles.label_servicos}>Código</label>
                         <input type="number" id="codigo_servico" name="codigo_servico" required className={styles.input_servicos} />
                     </div>
-                    <div className={`${styles.grid_item} ${styles.grid_item_large}`}>
-                        <label htmlFor="nome_servico" className={styles.label_servicos}>Nome</label>
-                        <input type="text" id="nome_servico" name="nome_servico" required className={styles.input_servicos} />
-                    </div>
-                    <div className={`${styles.grid_item} ${styles.grid_item_medium}`}>
-                        <label for="categoria_servico" className={styles.label_servicos}>Categoria:</label>
-                        <select id="categoria_servico" name="categoria_servico"
-                            className={`${styles.select} ${styles.grid_item_small}`}>
 
-                                
-                            <option value="manutencao">Manutenção</option>
-                            <option value="reparo">Reparo</option>
-                            <option value="inspecao">Inspeção</option>
-                            <option value="diagnostico">Diagnóstico</option>
-                            <option value="eletrico">Elétrico</option>
-                            <option value="pintura">Pintura</option>
-                            <option value="suspensao">Suspensão</option>
-                            <option value="freios">Freios</option>
-                            <option value="ar_condicionado">Ar Condicionado</option>
-                            <option value="limpeza">Limpeza</option>
+                    <div className={`${styles.grid_item} ${styles.grid_categoria}`}>
+                        <label for="categoria_servico" className={styles.label_servicos}>Categoria</label>
+                        <select id="categoria_servico" name="categoria_servico"
+                            className={`${styles.select_servicos} ${styles.grid_categoria}`}>
+                            <option value="" disabled selected>Selecionar</option>
+                            <option value="1" className={styles.option}>Lavagem e Higienizacao</option>
+                            <option value="2" className={styles.option}>Polimento e Protecao da Pintura</option>
+                            <option value="3" className={styles.option}>Cuidados com Vidros e Farois</option>
+                            <option value="4" className={styles.option}>Cuidados com Rodas e Pneus</option>
+                            <option value="5" className={styles.option}>Detalhamento Interno (tailing)</option>
+                            <option value="6" className={styles.option}>Remocao de Odores</option>
+                            <option value="7" className={styles.option}>Correcao de Pintura</option>
+                            <option value="8" className={styles.option}>Customizacao</option>
+                            <option value="9" className={styles.option}>Reparos Esteticos</option>
+                            <option value="10" className={styles.option}>Servicos Adicionais</option>
                         </select>
                     </div>
-                    <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
-                        <label htmlFor="duracao_servico" className={styles.label_servicos}>Duração Estimada:</label>
+
+                    <div className={`${styles.grid_item} ${styles.grid_nome}`}>
+                        <label htmlFor="nome_servico" className={styles.label_servicos}>Nome</label>
+                        <select id="categoria_servico" name="categoria_servico"
+                            className={`${styles.select_servicos} ${styles.input_nome}`}>
+                            <option value="" disabled selected>Selecionar</option>
+                            <option value="1" className={styles.option}>Lavagem externa (tradicional)</option>
+                            <option value="2" className={styles.option}>Lavagem externa (a seco)</option>
+                            <option value="3" className={styles.option}>Lavagem interna completa</option>
+                            <option value="4" className={styles.option}>Limpeza de estofados</option>
+                            <option value="5" className={styles.option}>Limpeza de carpetes</option>
+                            <option value="6" className={styles.option}>Higienizacao de ar-condicionado</option>
+                            <option value="7" className={styles.option}>Descontaminacao de pintura</option>
+                            <option value="8" className={styles.option}>Polimento de pintura</option>
+                            <option value="9" className={styles.option}>Espelhamento</option>
+                            <option value="10" className={styles.option}>Vitrificacao</option>
+                            <option value="11" className={styles.option}>Enceramento</option>
+                            <option value="12" className={styles.option}>Selagem de pintura</option>
+                            <option value="13" className={styles.option}>Aplicacao de pelicula de protecao de pintura (PPF)</option>
+                            <option value="14" className={styles.option}>Polimento de farois</option>
+                            <option value="15" className={styles.option}>Restauracao de farois</option>
+                            <option value="16" className={styles.option}>Aplicacao de repelente de agua (cristalizacao de vidros)</option>
+                            <option value="17" className={styles.option}>Aplicacao de insulfilm</option>
+                            <option value="18" className={styles.option}>Limpeza de rodas e calotas</option>
+                            <option value="19" className={styles.option}>Polimento de rodas</option>
+                            <option value="20" className={styles.option}>Aplicacao de protetores de pneu</option>
+                            <option value="21" className={styles.option}>Pintura e restauracao de rodas</option>
+                            <option value="22" className={styles.option}>Limpeza detalhada do painel e console</option>
+                            <option value="23" className={styles.option}>Limpeza e hidratacao de bancos de couro</option>
+                            <option value="24" className={styles.option}>Limpeza e hidratacao de plasticos internos</option>
+                            <option value="25" className={styles.option}>Limpeza de portas e macanetas</option>
+                            <option value="26" className={styles.option}>Detalhamento de dutos de ventilacao</option>
+                            <option value="27" className={styles.option}>Tratamento com ozonio</option>
+                            <option value="28" className={styles.option}>Neutralizacao de odores</option>
+                            <option value="29" className={styles.option}>Retoque de pintura</option>
+                            <option value="30" className={styles.option}>Pintura parcial</option>
+                            <option value="31" className={styles.option}>Envelopamento automotivo (vinil)</option>
+                            <option value="32" className={styles.option}>Pintura de pincas de freio</option>
+                            <option value="33" className={styles.option}>Aplicacao de adesivos decorativos</option>
+                            <option value="34" className={styles.option}>Instalacao de spoilers e acessorios externos</option>
+                            <option value="35" className={styles.option}>Remocao de riscos e arranhoes</option>
+                            <option value="36" className={styles.option}>Reparo de amassados sem pintura (martelinho de ouro)</option>
+                            <option value="37" className={styles.option}>Remocao de manchas</option>
+                            <option value="38" className={styles.option}>Lavagem de motor</option>
+                            <option value="39" className={styles.option}>Polimento de escapamento</option>
+                            <option value="40" className={styles.option}>Revitalizacao de teto solar</option>
+                        </select>
+                    </div>
+
+                    <div className={`${styles.grid_item} ${styles.grid_duracao}`}>
+                        <label htmlFor="duracao_servico" className={styles.label_servicos}>Duração Estimada</label>
                         <select id="duracao_servico" name="duracao_servico"
-                            className={`${styles.select} ${styles.grid_item_small}`}>
-                            <option value="30">30 minutos</option>
+                            className={`${styles.select_servicos} ${styles.input_duracao}`}>
+                            <option value="" disabled selected>Selecionar</option>
                             <option value="60">1 hora</option>
-                            <option value="90">1 hora e 30 minutos</option>
                             <option value="120">2 horas</option>
                             <option value="180">3 horas</option>
                             <option value="240">4 horas</option>
@@ -57,22 +101,22 @@ export default function Servicos() {
                             <option value="480">8 horas</option>
                         </select>
                     </div>
-                    <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
-                        <label htmlFor="preco_servico" className={styles.label_servicos}>Preço:</label>
+                    <div className={`${styles.grid_item} ${styles.grid_preco}`}>
+                        <label htmlFor="preco_servico" className={styles.label_servicos}>Preço</label>
                         <input type="number" id="preco_servico" name="preco_servico" required className={styles.input_servicos} />
                     </div>
-                    <div className={`${styles.grid_item} ${styles.grid_item_large} ${styles.grid_item_descricao}`}>
+                    <div className={`${styles.grid_item} ${styles.grid_descricao} ${styles.grid_item_descricao}`}>
                         <label htmlFor="descricao_servico" className={styles.label_servicos}>Descrição</label>
                         <input type="text" id="descricao_servico" name="descricao_servico" required className={styles.input_servicos} />
                     </div>
-                    <div className={`${styles.grid_item} ${styles.grid_item_large} ${styles.grid_item_observacoes}`}>
+                    <div className={`${styles.grid_item} ${styles.grid_observacoes} ${styles.grid_item_observacoes}`}>
                         <label htmlFor="observacoes_servico" className={styles.label_servicos}>Observações</label>
                         <input type="text" id="observacoes_servico" name="observacoes_servico" required className={styles.input_servicos} />
                     </div>
-                    <div className={`${styles.grid_item} ${styles.grid_item_small}`}>
-                        <label for="situacao_servico" className={styles.label_servicos}>Situação:</label>
+                    <div className={`${styles.grid_item} ${styles.grid_situacao}`}>
+                        <label for="situacao_servico" className={styles.label_servicos}>Situação</label>
                         <select id="situacao_servico" name="situacao_servico"
-                            className={`${styles.select} ${styles.grid_item_small}`}>
+                            className={`${styles.select_servicos} ${styles.input_situacao}`}>
                             <option value="ativo" className={styles.option} selected>Ativo</option>
                             <option value="inativo" className={styles.option}>Inativo</option>
                         </select>
